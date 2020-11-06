15adb9cd0f02709acf44d069f3069dd6fd3c8e23
khouloud711 <bejaouikhouloud98@gmail.com>
2020-11-06 13:18:45 +0100

    home buttons

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,21 +2,23 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 
 
+
 import './Home.css';
 
 export const  Home = (props) => {
 
     return (
         <div>
+           
             <h1>Tic Tac Toe</h1>
             <Link to='/Human-VS-Humain' style={{ textDecoration: 'none' }}>
                 <div id="new-game-container">
-                    <button id="new-game-button">Humain vs Humain</button>
+                    <button id="new-game-button"><i class="fa fa-user"></i> vs <i class="fa fa-user"></i></button>
                 </div>
             </Link>
             <Link to='/Human-VS-Computer' style={{ textDecoration: 'none' }}>
                 <div id="new-game-container">
-                    <button id="new-game-button">Humain vs Computer</button>
+                    <button id="new-game-button"><i class="fa fa-user"></i> vs <i class="fa fa-robot"></i></button>
                 </div>
             </Link>
         </div>
